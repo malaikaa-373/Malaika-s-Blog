726eedaebb23bdfad20053858477f650ca9206b4
Malaika <malaikaa373@google.com>
2026-06-28 20:23:22 +0500

    fix

--- Mega-Project/src/components/index.js
+++ Mega-Project/src/components/index.js
@@ -5,7 +5,6 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import SignUp from "./SignUp";
-import Signup from "./Signup";
 import Login from "./Login";
 import PostForm from './PostForm';
 import PostCard from './PostCard';
