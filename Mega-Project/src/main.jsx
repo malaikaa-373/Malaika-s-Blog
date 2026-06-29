3bc7b80825e7c0183faf71ffdd5a7beeb8825ef1
Malaika <malaikaa373@google.com>
2026-06-29 07:03:11 +0500

    fix

--- Mega-Project/src/main.jsx
+++ Mega-Project/src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import store from './store/Store.js'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from "./pages/Home.jsx";
-import { AuthLayout, Login, Signup } from "./components/index.js";
+import { AuthLayout, Login, SignUp } from "./components/index.js";
 import AllPosts from "./pages/AllPosts.jsx";
 import AddPost from "./pages/AddPost.jsx";
 import EditPost from "./pages/EditPost.jsx";
